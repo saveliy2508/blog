a520cd5d99088fda8a7a176cfba3f0094119ac96
Saveliy <svolskey2929@gmail.com>
2022-09-18 11:57:54 +0300

    webpack

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,13 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+import {BuildPaths} from "./types/config";
+
+export function buildPlugins(paths: BuildPaths): webpack.WebpackPluginInstance[]{
+
+    return [
+        new HtmlWebpackPlugin({
+            template: paths.html
+        }),
+        new webpack.ProgressPlugin(),
+    ]
+}
