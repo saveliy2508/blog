c44f7d7966ba1db86d88bc43390aff60ee259862
Saveliy <svolskey2929@gmail.com>
2022-11-19 01:41:05 +0300

    anime

--- src/app/providers/ThemeProvider/lib/ThemeContext.ts
+++ src/app/providers/ThemeProvider/lib/ThemeContext.ts
@@ -2,7 +2,8 @@ import { createContext } from 'react';
 
 export enum Theme {
     LIGHT = 'light',
-    DARK = 'dark'
+    DARK = 'dark',
+    ANIME = 'anime'
 }
 
 export interface ThemeContextProps {
